ec088b34445f714f808c4b09634a8ba67c23f264
nnu1cob <Natrayan.Anusha@in.bosch.com>
2022-03-29 18:26:09 +0530

    Test cases are added

--- TypewiseAlert.Test/TypewiseAlertTest.cs
+++ TypewiseAlert.Test/TypewiseAlertTest.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using static TypewiseAlert.TypewiseAlert;
 
 namespace TypewiseAlert.Test
 {
@@ -9,6 +10,28 @@ namespace TypewiseAlert.Test
         public static void InfersBreachAsPerLimits()
         {
             Assert.True(TypewiseAlert.InferBreach(12, 20, 30) == TypewiseAlert.BreachType.TooLow);
+        }
+
+        [Fact]
+        public static void ClasifyTemperatureBreachAsPerLimits()
+        {
+            Assert.True(TypewiseAlert.ClassifyTemperatureBreach(CoolingType.PassiveCooling, 30.0) == TypewiseAlert.BreachType.Normal);
+        }
+
+        [Fact]
+        public static void CheckAndAlertToEmail()
+        {
+            BatteryCharacter batteryChar = new BatteryCharacter();
+            batteryChar.coolingType = CoolingType.HighActiveCooling;
+            TypewiseAlert.CheckAndAlert(AlertTarget.ToEmail, batteryChar, 40.5);
+        }
+
+        [Fact]
+        public static void CheckAndAlertToController()
+        {
+            BatteryCharacter batteryChar = new BatteryCharacter();
+            batteryChar.coolingType = CoolingType.MediumActiveCooling;
+            TypewiseAlert.CheckAndAlert(AlertTarget.ToController, batteryChar, 20.0);
         }
     }
 }
